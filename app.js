89de0e9dd7180b737174280f6d3d27c3731b71ab
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-01 17:03:09 -0500

    adds weather api

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const https = require("https");
+const { url } = require("inspector");
 
 const app = express();
 
 app.get("/", function(req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e72ca729af228beabd5d20e3b7749713&units=metric";
+    https.get(url);
     res.send("Server is up and running.");
 })
 
